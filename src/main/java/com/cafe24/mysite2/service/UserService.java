d9899d606f840a27ae8b70fce1d8194b42624d5a
Soobinnn <isb9082@naver.com>
2019-05-16 17:07:31 +0900

    0516실습

---
+++ src/main/java/com/cafe24/mysite2/service/UserService.java
@@ -0,0 +1,29 @@
+package com.cafe24.mysite2.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cafe24.mysite2.repository.UserDao;
+import com.cafe24.mysite2.vo.UserVo;
+
+
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserDao userDao;
+	
+	public Boolean join(UserVo userVo) {
+		return userDao.insert(userVo);
+	}
+
+	public UserVo getUser(Long userNo) {
+		return null;
+	}
+	
+	public UserVo getUser(UserVo userVo) {
+		return userDao.get(userVo.getEmail(), userVo.getPassword());
+	}
+	
+}
